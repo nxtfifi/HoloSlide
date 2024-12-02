b000ac8dc3a2e4c83f5623e15bfd33191a95ed53
nxtfifi <nxtfifi@gmail.com>
2024-12-01 19:16:34 -0600

    Declaracion variables
    Declaramos las variables que vamos a usar en un futuro

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ camara=cv2.VideoCapture(0)
 camara.set(3,ancho) #Ancho del cuadro
 camara.set(4,alto) #Alto del cuadro
 detector_manos=HandDetector(detectionCon=0.8,maxHands=1)
+
+lista_imagenes=[]
+retraso=30
+boton_presionado=False
+contador=0
+modo_dibujo=False
+numero_imagen=0
+contador_retraso=0
+anotaciones=[[]] #Lista para guardas anotaciones
+numero_anotacion=-1 #Indice de anotacion actual
+inicio_anotacion=False
+alto_pequena, ancho_pequena=int(120*1),int(213*1) #Dimensiones de la vista previa
+
