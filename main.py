7b58dd7dffef12d37427b70b724b5d01316477c4
nxtfifi <nxtfifi@gmail.com>
2024-12-01 19:06:45 -0600

    Configuracion camara
    Establecemos el ancho y alto del cuadro de la camara,así como establecemos los limites de la cantidad de manos que puede detectar

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ ancho, alto=1280,720 #Resolución de la pantalla
 umbral_gesto=360 #Linea para detectar los gestos
 carpeta_presentacion="presentation" #Es la carpeta donde estaran los png
 
+camara=cv2.VideoCapture(0)
+camara.set(3,ancho) #Ancho del cuadro
+camara.set(4,alto) #Alto del cuadro
+detector_manos=HandDetector(detectionCon=0.8,maxHands=1)
