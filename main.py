d6e3748370fffca8a3948ea51602cdad357a119c
nxtfifi <nxtfifi@gmail.com>
2024-12-01 19:01:10 -0600

    Parametros iniciales
    Aqui definimos la resolcion de la pantalla al igual que definimos la linea para la deteccion de gestos y creamos la carpeta donde iran guardadas las diapositivas

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import cv2
 import os
 import numpy as np
 
+#Parmetros iniciales
+ancho, alto=1280,720 #Resolución de la pantalla
+umbral_gesto=360 #Linea para detectar los gestos
+carpeta_presentacion="presentation" #Es la carpeta donde estaran los png
+
